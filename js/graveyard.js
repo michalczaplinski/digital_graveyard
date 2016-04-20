bb79c79a1ef87be0dacf658bee290dec3ace6b17
Michal Czaplinski <mmczaplinski@gmail.com>
2016-04-20 23:29:54 +0200

    update to the code getting the data

--- js/graveyard.js
+++ js/graveyard.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         return new_date.toLocaleString()
     }
 
-    $.getJSON('../data/tweets.json', function(json, textStatus) {
+    $.getJSON('tweets.json', function(json, textStatus) {
         console.log(textStatus)
 
         var myJSONString = JSON.stringify(json);
